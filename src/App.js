9c568dc7471788394f61f0ecd0fadef8e3fc221d
Erik Lopes <erik.lopesdev@gmail.com>
2021-10-18 17:48:09 -0300

    Exercicio 01 ao 05

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 import User from './components/Users'
+import About from "./components/About";
 import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -8,10 +9,9 @@ class App extends Component {
     return (
       <>
       <BrowserRouter>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/user/:id" render={(props) => <User {...props} greetingsMessage="Good Morning"  />} />
+        <Route exact path="/" render={(props) => <Home {...props}/>}/>
+        <Route path="/about" render={(props) => <About {...props} /> }/>
+        <Route path="/users/:id" render={(props) => <User {...props} greetingsMessage="Good Morning"  />} />
       </BrowserRouter>
       </>
     );
